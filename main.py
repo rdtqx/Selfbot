548a0498c79c6e29ec04a45b7ebc9a458187b565
rdtqx <getspammedyaneek@gmail.com>
2025-05-28 03:46:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ client = discord.Client(intents=intents)
 start_time = None
 
 # === Game Info ===
-GAME_NAME = "bust a nut simulator"
+GAME_NAME = "bust a nut simulator 2021"
 GAME_IMAGE_URL = "https://cdn.discordapp.com/attachments/1342843098274861087/1377112134470598707/IMG_4919.jpg?ex=6837c71e&is=6836759e&hm=a4326291fea3ad8397e95a465796f73903515bc6a3a7efd601aaf6a3b7c97b2c&"  # Replace with your image
 
 # === Format Uptime ===
